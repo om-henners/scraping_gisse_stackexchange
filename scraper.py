e78ab9c3d8745607406962cbf12c31210e019c13
om-henners <henry.walshaw@gmail.com>
2014-06-26 14:17:05 +1000

    Updating page count to get every page on GIS SE
    Ideally this should keep going until it finds no more users, or the page count maxes out.

--- scraper.py
+++ scraper.py
@@ -2,23 +2,28 @@
 #and then map them, taken from question http://meta.gis.stackexchange.com/questions/544/map-of-user-locations
 
 import scraperwiki
-from BeautifulSoup import BeautifulSoup
+from bs4 import BeautifulSoup
 import re
 
 #looping through the user pages
-for i in (range(5)): #a bit hacky, if you want all the user pages, just insert 77 (as of 7/26/2011)!
+
+user_details = True #cheat so that the scraper starts
+i = 1
+
+while user_details and i < 1000: #want this to time out eventually...
     url = "http://gis.stackexchange.com/users?page=" + str(i)
     webpage = scraperwiki.scrape(url)
     soup = BeautifulSoup(webpage)
     user_details = soup.findAll("div","user-details") #within this set of tags is the info I want
 
 #scraping the user info 
-    for i in range(len(user_details)):
-        temp_name = user_details[i].find(name = 'a',attrs={"href" : re.compile("/users/")})    
+    for user in user_details:
+        temp_name = user.find(name='a', attrs={"href" : re.compile("/users/")})    
         data = {}
         data ['name'] = temp_name.string
-        temp_location = user_details[i].find("span", "user-location")
+        temp_location = user.find("span", "user-location")
         data['location'] = temp_location.string
-        scraperwiki.sqlite.save(unique_keys=['name'],data=data)    #saving the data
+        scraperwiki.sqlite.save(unique_keys=['name'], data=data)    #saving the data
 
+    i += 1
 
