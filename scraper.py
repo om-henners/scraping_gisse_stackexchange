53eadac1816edced16d4650df65e13924d923dea
Henry Walshaw <henry.walshaw@gmail.com>
2014-06-28 16:10:28 +1000

    Re-write to use APIs
    Now uses the stackexcahnge API and the geonames API
    
    Signed-off-by: Henry Walshaw <henry.walshaw@gmail.com>

--- scraper.py
+++ scraper.py
@@ -1,32 +1,109 @@
 #this scraper is intended to grab the addresses listed on the stackexchange profile pages,
 #and then map them, taken from question http://meta.gis.stackexchange.com/questions/544/map-of-user-locations
 
-import scraperwiki
-from bs4 import BeautifulSoup
-import re
+import os
+import requests
+import functools
+import geopy
+import sqlite3
+import time
 
-#looping through the user pages
+def memoize(obj):
+    #From https://wiki.python.org/moin/PythonDecoratorLibrary#Memoize
+    cache = obj.cache = {}
 
-user_details = True #cheat so that the scraper starts
-i = 1
+    @functools.wraps(obj)
+    def memoizer(*args, **kwargs):
+        key = str(args) + str(kwargs)
+        if key not in cache:
+            cache[key] = obj(*args, **kwargs)
+        return cache[key]
+    return memoizer
 
-while user_details and i < 1000: #want this to time out eventually...
-    print "Scraping page {}".format(i)
-    url = "http://gis.stackexchange.com/users?page=" + str(i)
-    webpage = scraperwiki.scrape(url)
-    soup = BeautifulSoup(webpage)
-    user_details = soup.findAll("div","user-details") #within this set of tags is the info I want
+class LimitRequestsPerHour(object):
+    def __init__(self, max_requests_per_hour):
+        self.max_requests = max_requests_per_hour
+        self.count = 0
+    def __call__(self, obj):
+        def wrapee(*args, **kwargs):
+            if self.count > self.max_requests:
+                print "Sleeping for an hour"
+                time.sleep(60*60) #pause for an hour
+                self.count = 1
+            else:
+                self.count += 1
+            print "Request {}/{}".format(self.count, self.max_requests)
+            return obj(*args, **kwargs)
+        return wrapee
 
-#scraping the user info 
-    for user in user_details:
-        temp_name = user.find(name='a', attrs={"href" : re.compile("/users/")})
-        data = {}
-        data['name'] = unicode(temp_name.string)
-        temp_location = user.find("span", "user-location")
-        data['location'] = unicode(temp_location.string)
-        #print data
-        scraperwiki.sqlite.save(unique_keys=['name'], data=data)    #saving the data
+@memoize
+@LimitRequestsPerHour(1900)
+def resolve_location(location_name):
+    if location_name in {None, "-", "None"}:
+        return None, None #Neither longitude or latitude should be returned
+    print "Resolving location {}".format(location_name)
+    gn = geopy.GeoNames(username=os.environ["MORPH_GEONAMES_USERNAME"])
+    resolved_name, (latitude, longitude) = gn.geocode(location_name)
+    print "Resolved as {}".format(resolved_name)
+    return longitude, latitude
 
-    i += 1
 
-#TODO: Register an account at geonames.org, and look at gelocating place names
+def get_gis_se_users():
+    stack_exchange_users = "http://api.stackexchange.com/2.2/users"
+    stack_exchange_params = {
+        "order": "desc",
+        "sort": "reputation",
+        "filter": "default",
+        "site": "gis"
+    }
+    if "MORPH_SE_KEY" in os.environ:
+        stack_exchange_params["key"] = os.environ["MORPH_SE_KEY"]
+
+    print "Searching users"
+
+    for page in xrange(1, 1000): #At some point we want to stop
+        print "Requesting page {}".format(page)
+        stack_exchange_params["page"] = page
+        r = requests.get(stack_exchange_users, params=stack_exchange_params)
+        users = r.json()
+
+
+        for user in users["items"]:
+            print user
+            user_id = user["user_id"]
+            location_name = user.get("location", None)
+            display_name = user["display_name"]
+
+            longitude, latitude = resolve_location(location_name)
+
+            yield user_id, display_name, location_name, longitude, latitude
+
+        if not users["has_more"]:
+            break
+
+
+def scrape_data():
+    conn = sqlite3.connect("data.sqlite")
+    conn.execute("""drop table if exists data""")
+    conn.commit()
+    with conn:
+
+        conn.execute("""
+        create table if not exists data (
+        id int primary key on conflict replace,
+        display_name varchar,
+        location varchar,
+        longitude float,
+        latitude float
+        )
+        """)
+        conn.commit()
+        conn.executemany(
+            """insert into data(id, display_name, location, longitude, latitude) values (?, ?, ?, ?, ?)""",
+            get_gis_se_users()
+        )
+
+
+if __name__ == "__main__":
+    print "Let's go"
+    scrape_data()
